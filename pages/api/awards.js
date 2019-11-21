222f2423f0c75b196d2c3fda802a59de45894ccf
Brendan Nee <brendan@blinktag.com>
2019-11-20 23:14:18 -0800

    Add awards endpoint

---
+++ pages/api/awards.js
@@ -0,0 +1,9 @@
+import { airtableGet } from '../../lib/airtable'
+
+export default async (req, res) => {
+  const awards = await airtableGet('Awards', {
+    filterByFormula: 'IF(OR(NOT({Award Amount} = ""),NOT({Project} = "")), 1, 0)',
+    'sort[0][field]': 'Date'
+  })
+  res.status(200).json(awards)
+}
