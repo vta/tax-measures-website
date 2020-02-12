4d78f9e5e74734f5c86ddd2a9353c486314b80a8
Brendan Nee <brendan@blinktag.com>
2020-02-12 13:38:56 -0800

    Allow awards without projects

--- pages/api/awards.js
+++ pages/api/awards.js
@@ -2,7 +2,7 @@ import { airtableGet } from '../../lib/airtable'
 
 export default async (req, res) => {
   const awards = await airtableGet('Awards', {
-    filterByFormula: 'IF(OR(NOT({Award Amount} = ""),NOT({Project} = "")), 1, 0)',
+    filterByFormula: 'NOT({Award Amount} = "")',
     'sort[0][field]': 'Date'
   })
   res.status(200).json(awards)
