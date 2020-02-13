ca007de15001436cf38d68566063581eab1ea9ff
Brendan Nee <brendan@blinktag.com>
2020-02-12 17:12:34 -0800

    Remove projects from awards and payments

--- pages/api/payments.js
+++ pages/api/payments.js
@@ -2,7 +2,7 @@ import { airtableGet } from '../../lib/airtable'
 
 export default async (req, res) => {
   const payments = await airtableGet('Payments', {
-    filterByFormula: 'IF(OR(NOT({Amount} = ""),NOT({Project} = "")), 1, 0)',
+    filterByFormula: 'NOT({Amount} = "")',
     'sort[0][field]': 'Date'
   })
   res.status(200).json(payments)
