3dcae278c0566bb3cfa8c5ec1cf102af1d5358d1
Brendan Nee <brendan@blinktag.com>
2020-03-11 15:13:03 -0700

    Ignore allocations with no category

--- pages/api/allocations.js
+++ pages/api/allocations.js
@@ -3,7 +3,7 @@ import { airtableGet } from '../../lib/airtable'
 export default async (req, res) => {
   const allocations = await airtableGet('Allocations', {
     'fields[]': ['Allocation ID', 'Amount', 'Category', 'Date Allocated', 'Available Start', 'Project'],
-    filterByFormula: 'NOT({Amount} = "")',
+    filterByFormula: 'IF(OR(NOT({Amount} != ""), NOT({Category} = BLANK())), 1, 0)',
     'sort[0][field]': 'Date Allocated'
   })
   res.status(200).json(allocations)
