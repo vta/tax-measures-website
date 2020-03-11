0f0686b3d0d24ef7a0fc1822811f88490fc5d029
Brendan Nee <brendan@blinktag.com>
2020-03-10 22:03:39 -0700

    Simplifiy allocation

--- pages/api/allocations.js
+++ pages/api/allocations.js
@@ -2,7 +2,7 @@ import { airtableGet } from '../../lib/airtable'
 
 export default async (req, res) => {
   const allocations = await airtableGet('Allocations', {
-    'fields[]': ['Allocation ID', 'Amount', 'Category', 'Date Allocated', 'Available Start', 'Awards', 'Project'],
+    'fields[]': ['Allocation ID', 'Amount', 'Category', 'Date Allocated', 'Available Start', 'Project'],
     filterByFormula: 'NOT({Amount} = "")',
     'sort[0][field]': 'Date Allocated'
   })
