126d862b8ad404afc590cfc65ba6fb0603a5795f
Brendan Nee <brendan@blinktag.com>
2020-05-11 23:08:34 -0700

    Update to next.js 9.4

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import '../node_modules/bootstrap/scss/bootstrap.scss'
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
+import '../node_modules/mapbox-gl/dist/mapbox-gl.css'
+import '../css/index.scss'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
