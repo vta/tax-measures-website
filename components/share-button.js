29d530de59336701ea954be62922e653a706b1d2
Brendan Nee <brendan@blinktag.com>
2020-05-21 14:38:00 -0700

    lints

--- components/share-button.js
+++ components/share-button.js
@@ -4,17 +4,17 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { faCopy, faEnvelope, faShare } from '@fortawesome/free-solid-svg-icons'
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const ShareButton = ({ className }) => {
   const emailShareUrl = `mailto:?subject=2016%20Measure%20B&body=${encodeURIComponent(window.location.href)}`
   const twitterShareUrl = `https://twitter.com/intent/tweet/?text=${encodeURIComponent('2016 Measure B')}&amp;url=${encodeURIComponent(window.location.href)}`
   const facebookShareUrl = `https://facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`
-  
+
   return (
     <Dropdown>
       <Dropdown.Toggle className={className}>
-      <FontAwesomeIcon icon={faShare} title="Share" /> Share
+        <FontAwesomeIcon icon={faShare} title="Share" /> Share
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
