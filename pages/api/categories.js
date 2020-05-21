ad7d0534d733ae75c1d7eadaee26ad6e1ea14031
Brendan Nee <brendan@blinktag.com>
2020-05-20 17:47:54 -0700

    Category cards at top

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -2,7 +2,7 @@ import { airtableGet } from '../../lib/airtable'
 
 export default async (request, response) => {
   const categories = await airtableGet('Categories', {
-    'fields[]': ['Name', 'Parent Category', 'Ballot Allocation'],
+    'fields[]': ['Name', 'Description', 'Parent Category', 'Ballot Allocation'],
     filterByFormula: 'NOT({Name} = "")',
     'sort[0][field]': 'Name'
   })
