708655b0d2f16dbaf9d723c0cc9c239ce2dd7da6
Brendan Nee <brendan@blinktag.com>
2020-05-22 14:00:35 -0700

    Share  dialog

--- components/share-button.js
+++ components/share-button.js
@@ -7,9 +7,22 @@ import { faCopy, faEnvelope, faShare } from '@fortawesome/free-solid-svg-icons'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const ShareButton = ({ className }) => {
-  const emailShareUrl = `mailto:?subject=2016%20Measure%20B&body=${encodeURIComponent(window.location.href)}`
-  const twitterShareUrl = `https://twitter.com/intent/tweet/?text=${encodeURIComponent('2016 Measure B')}&amp;url=${encodeURIComponent(window.location.href)}`
-  const facebookShareUrl = `https://facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`
+  const shareTitle = '2016 Measure B'
+  const shareUrl = window.location.href
+  const emailShareUrl = `mailto:?subject=2016%20Measure%20B&body=${encodeURIComponent(shareUrl)}`
+  const twitterShareUrl = `https://twitter.com/intent/tweet/?text=${encodeURIComponent(shareTitle)}&amp;url=${encodeURIComponent(shareUrl)}`
+  const facebookShareUrl = `https://facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareUrl)}`
+
+  function triggerShare(event) {
+    if (navigator.share) {
+      event.preventDefault()
+      navigator.share({
+        title: shareTitle,
+        url: shareUrl
+      })
+      .catch(console.error)
+    }
+  }
 
   return (
     <Dropdown>
@@ -19,11 +32,11 @@ const ShareButton = ({ className }) => {
 
       <Dropdown.Menu>
         <Dropdown.Item>
-          <CopyToClipboard text={window.location.href}>
+          <CopyToClipboard text={shareUrl}>
             <span><FontAwesomeIcon icon={faCopy} title="Copy" /> Copy URL</span>
           </CopyToClipboard>
         </Dropdown.Item>
-        <Dropdown.Item href={emailShareUrl} target="_blank"><FontAwesomeIcon icon={faEnvelope} title="Email" /> Email</Dropdown.Item>
+        <Dropdown.Item href={emailShareUrl} onClick={triggerShare} target="_blank"><FontAwesomeIcon icon={faEnvelope} title="Email" /> Email</Dropdown.Item>
         <Dropdown.Item href={twitterShareUrl} target="_blank"><FontAwesomeIcon icon={faTwitter} title="Twitter" /> Twitter</Dropdown.Item>
         <Dropdown.Item href={facebookShareUrl} target="_blank"><FontAwesomeIcon icon={faFacebook} title="Facebook" /> Facebook</Dropdown.Item>
       </Dropdown.Menu>
