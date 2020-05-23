1c08b15b18369f8c8322fbbbf467d693baf6c1bb
Brendan Nee <brendan@blinktag.com>
2020-05-23 00:44:48 -0700

    Filter alert

---
+++ components/filter-alert.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Alert from 'react-bootstrap/Alert'
+import { compact } from 'lodash'
+
+const FilterAlert = ({ results, currentFilters }) => {
+  const filterCount = currentFilters ? compact(Object.values(currentFilters)).length : 0
+  if (!results) {
+    return null
+  }
+
+  if (results.items.length === 0) {
+    return (
+      <Alert variant="warning" className="text-center">
+        <Alert.Heading>No funded projects meet these criteria.</Alert.Heading>
+        <div>Try adjusting search filters.</div>
+      </Alert>
+    )
+  }
+
+  if (results.items.length < 5) {
+    return (
+      <Alert variant="warning" className="text-center">
+        <Alert.Heading>Limited results</Alert.Heading>
+        <div>Consider broadening your search if you're not seeing enough results. Select a broader date range or choose additional categories, grantees or projects.</div>
+      </Alert>
+    )
+  }
+
+  if (filterCount < 2) {
+    return (
+      <Alert variant="warning" className="text-center">
+        <Alert.Heading>Numerous results</Alert.Heading>
+        <div>Consider selecting additional filters to narrow down your results or focus on the information you're interested in.</div>
+      </Alert>
+    )
+  }
+
+  return null
+}
+
+export default FilterAlert
