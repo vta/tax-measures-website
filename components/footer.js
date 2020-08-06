de3f82ca537371dfb82be5e78c631cd090cf95ba
Brendan Nee <brendan@blinktag.com>
2020-08-06 11:53:37 -0700

    Footer and about page content changes

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ const Footer = () => (
           </div>
         </div>
         <div className="col-md-3 mb-3">
-          <p>This website provides financial information about VTA's 2016 Measure B, updated quarterly or as new information is released.</p>
+          <p>This website provides unaudited financial information about VTA's 2016 Measure B, updated quarterly or as new information is released.</p>
           <a href="https://surveys.hotjar.com/s?siteId=1873169&surveyId=162313" target="_blank" className="text-white">Submit Feedback</a>
         </div>
         <div className="col-md-3 mb-3">
