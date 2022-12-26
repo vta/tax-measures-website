726863057803bda6062019915ab76319098ef132
Brendan Nee <brendan@blinktag.com>
2022-12-26 14:11:19 -0800

    SEO defaults

--- pages/index.js
+++ pages/index.js
@@ -154,15 +154,15 @@ const Home = ({ data }) => {
       <NextSeo
         title="2016 Measure B"
         description="A 30-year, half-cent countywide sales tax to enhance transit, highways, expressways and active transportation (bicycles, pedestrians and complete streets). The measure passed by nearly 72%, the highest level of support for any Santa Clara County transportation tax."
-        canonical="https://2016measureb-beta.vta.org/"
+        canonical="https://2016measureb.vta.org/"
         openGraph={{
-          url: 'https://2016measureb-beta.vta.org/',
+          url: 'https://2016measureb.vta.org/',
           title: '2016 Measure B',
           description:
             '"A 30-year, half-cent countywide sales tax to enhance transit, highways, expressways and active transportation (bicycles, pedestrians and complete streets). The measure passed by nearly 72%, the highest level of support for any Santa Clara County transportation tax.',
           images: [
             {
-              url: 'https://2016measureb-beta.vta.org/meta/measureb-logo-square.png',
+              url: 'https://2016measureb.vta.org/meta/measureb-logo-square.png',
               width: 1200,
               height: 1200,
               alt: '2016 Measure B',
