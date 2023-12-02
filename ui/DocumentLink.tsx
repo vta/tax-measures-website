1ff7ec632ea5138ae7f54972f908ef3cf9092989
Brendan Nee <brendan@blinktag.com>
2023-12-02 11:31:26 -0800

    Remove attachments from request

--- ui/DocumentLink.tsx
+++ ui/DocumentLink.tsx
@@ -11,33 +11,5 @@ export const DocumentLink = ({ document }) => {
     );
   }
 
-  if (document.fields.Attachment && document.fields.Attachment.length === 1) {
-    return (
-      <a
-        href={document.fields.Attachment[0].url}
-        target="_blank"
-        rel="noreferrer"
-      >
-        {document.fields.Name}{' '}
-        <FontAwesomeIcon icon={faFileDownload} size="xs" />
-      </a>
-    );
-  }
-
-  if (document.fields.Attachment) {
-    return document.fields.Attachment.map((attachment, index) => (
-      <a
-        href={attachment.url}
-        target="_blank"
-        key={index}
-        className="mr-4"
-        rel="noreferrer"
-      >
-        {index === 0 ? document.fields.Name : attachment.filename}{' '}
-        <FontAwesomeIcon icon={faFileDownload} size="xs" />
-      </a>
-    ));
-  }
-
   return document.fields.Name;
 };
