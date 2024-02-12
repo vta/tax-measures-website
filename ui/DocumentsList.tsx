5dc83040eaabedc73b3098db0788f5e7fe9bb75a
Brendan Nee <brendan@blinktag.com>
2024-02-12 15:53:41 -0800

    Document type selector

--- ui/DocumentsList.tsx
+++ ui/DocumentsList.tsx
@@ -1,64 +1,66 @@
 'use client';
-
 import { useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { sortBy } from 'lodash';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import AnimateHeight from 'react-animate-height';
+import { groupBy, sortBy } from 'lodash';
 
 import { DocumentLink } from '#/ui/DocumentLink';
 
 export const DocumentsList = ({ documents }) => {
-  const [slideDownHeight, setSlideDownHeight] = useState(0);
+  const sortedDocuments = sortBy(documents, 'createdTime').reverse();
+
+  const documentGroups = groupBy(
+    sortedDocuments,
+    (document) => document.fields['Document Type'],
+  );
+  const documentTypes = Object.keys(documentGroups)
+    .filter((documentType) => documentType !== undefined)
+    .sort();
+
+  const [selectedDocumentType, setSelectedDocumentType] = useState(
+    documentTypes?.[0],
+  );
+
   if (documents.length === 0) {
     return 'None';
   }
 
-  const sortedDocuments = sortBy(documents, 'createdTime').reverse();
-
-  return (
-    <>
+  if (documentTypes.length < 2) {
+    return (
       <ListGroup className="small-list-group">
-        {sortedDocuments.slice(0, 2).map((document) => (
+        {sortedDocuments.map((document) => (
           <ListGroup.Item key={document.id}>
             <DocumentLink document={document} />
           </ListGroup.Item>
         ))}
-        {sortedDocuments.length > 2 && (
-          <AnimateHeight
-            id="document-list"
-            duration={500}
-            height={slideDownHeight}
-          >
-            {sortedDocuments.slice(2).map((document) => (
-              <ListGroup.Item key={document.id}>
-                <DocumentLink document={document} />
-              </ListGroup.Item>
-            ))}
-          </AnimateHeight>
-        )}
       </ListGroup>
-      {sortedDocuments.length > 2 && (
-        <button
-          aria-expanded={slideDownHeight !== 0}
-          aria-controls="document-list"
-          onClick={() => setSlideDownHeight(slideDownHeight === 0 ? 'auto' : 0)}
-          className="btn btn-primary btn-sm mt-2"
+    );
+  }
+
+  return (
+    <div>
+      <label>
+        Document Type
+        <select
+          onChange={(event) => setSelectedDocumentType(event.target.value)}
+          className="ml-2"
         >
-          {slideDownHeight !== 0 ? (
-            <>
-              <FontAwesomeIcon icon={faChevronUp} className="mr-2" />
-              Show Fewer
-            </>
-          ) : (
-            <>
-              <FontAwesomeIcon icon={faChevronDown} className="mr-2" />
-              Show More
-            </>
-          )}
-        </button>
-      )}
-    </>
+          {documentTypes.map((documentType) => {
+            return <option value={documentType}>{documentType}</option>;
+          })}
+        </select>
+      </label>
+      <ListGroup className="small-list-group">
+        {sortedDocuments
+          .filter(
+            (document) =>
+              document.fields['Document Type'] === selectedDocumentType,
+          )
+          .map((document) => (
+            <ListGroup.Item key={document.id}>
+              <DocumentLink document={document} />
+            </ListGroup.Item>
+          ))}
+      </ListGroup>
+    </div>
   );
 };
