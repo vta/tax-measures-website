92926d84d50a517d214a0d97769053e420cae973
Brendan Nee <brendan@blinktag.com>
2025-05-05 20:48:25 -0700

    Remove numerous and limited warnings

--- ui/FilterAlert.tsx
+++ ui/FilterAlert.tsx
@@ -5,6 +5,7 @@ export const FilterAlert = ({ results, currentFilters }) => {
   const filterCount = currentFilters
     ? compact(Object.values(currentFilters)).length
     : 0;
+
   if (!results) {
     return null;
   }
@@ -14,31 +15,6 @@ export const FilterAlert = ({ results, currentFilters }) => {
       <Alert variant="warning" className="text-center">
         <Alert.Heading>No funded projects meet these criteria.</Alert.Heading>
         <div>Try adjusting search filters.</div>
-      </Alert>
-    );
-  }
-
-  if (results.projects.length < 5) {
-    return (
-      <Alert variant="warning" className="text-center">
-        <Alert.Heading>Limited results</Alert.Heading>
-        <div>
-          Consider broadening your search if you&apos;re not seeing enough
-          results. Select a broader date range or choose additional categories,
-          grantees or projects.
-        </div>
-      </Alert>
-    );
-  }
-
-  if (filterCount < 2) {
-    return (
-      <Alert variant="warning" className="text-center">
-        <Alert.Heading>Numerous results</Alert.Heading>
-        <div>
-          Consider selecting additional filters to narrow down your results or
-          focus on the information you&apos;re interested in.
-        </div>
       </Alert>
     );
   }
