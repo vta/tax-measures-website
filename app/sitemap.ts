b0ac07d0000f948f54cbdbeae782f7ff5010d9cd
Brendan Nee <brendan@blinktag.com>
2026-02-22 21:14:06 -0800

    Fix for sitemap priority

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 import { kebabCase } from 'lodash';
 
 import { fetchProjects } from '#/lib/api';
- 
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const projects = await fetchProjects();
-  const pages : MetadataRoute.Sitemap = [
+  const pages: MetadataRoute.Sitemap = [
     {
       url: 'https://2016measureb.vta.org',
       changeFrequency: 'monthly',
@@ -14,10 +14,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: 'https://2016measureb.vta.org/about',
       changeFrequency: 'monthly',
-      priority: 8,
+      priority: 0.8,
     },
-  ]
-  
+  ];
+
   for (const project of projects) {
     pages.push({
       url: `https://2016measureb.vta.org/projects/${kebabCase(project.fields.Name)}`,
